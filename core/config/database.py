41d34db2ec8f442af2a50048b18c9a967ad5b0da
H4CKER <aqdodovirshod7@gmail.com>
2026-01-13 13:14:41 +0500

    second commit

--- core/config/database.py
+++ core/config/database.py
@@ -1,10 +1,16 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite+aiosqlite:///./tasks.db"
+# Загрузка переменных окружения из .env
+load_dotenv()
+
+# Получение DATABASE_URL из .env или использование значения по умолчанию
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./tasks.db")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = async_sessionmaker(engine)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 async def get_session():
     async with async_session() as session:
